147bf86dc5e1d57298bddfb2228a17cfe7cf60cb
Marta Iachini <iachinimarta@gmail.com>
2022-08-25 18:38:58 +0200

    Prova inserimento card sezione bonus

--- src/components/MyMain.vue
+++ src/components/MyMain.vue
@@ -10,7 +10,14 @@
 
     <div class="bonus">
       <div class="container90">
-        Sezione bonus
+        <div class="card-container">
+          <ul>
+            <li v-for="(card, index) in cards" :key="index">
+              <img :src="card.img" :alt="card.text">
+              <h5>{{card.text}}</h5>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   </main>
@@ -18,7 +25,34 @@
 
 <script>
 export default {
-    name: 'MyMain'
+    name: 'MyMain',
+    data() {
+      return {
+        cards: [
+          {
+            img: require('../assets/img/buy-comics-digital-comics.png'),
+            text: 'DIGITAL COMICS'
+          },
+          {
+            img: require('../assets/img/buy-comics-merchandise.png'),
+            text: 'DC MERCHANDISE'
+          },
+          {
+            img: require('../assets/img/buy-comics-subscriptions.png'),
+            text: 'SUBSCRIPTION'
+          },
+          {
+            img: require('../assets/img/buy-comics-shop-locator.png'),
+            text: 'COMIC SHOP LOCATOR'
+          },
+          {
+            img: require('../assets/img/buy-dc-power-visa.svg'),
+            text: 'DC POWER VISA'
+          },
+
+        ]
+      }
+    }
 }
 </script>
 
@@ -41,6 +75,19 @@ export default {
     background-color: #0282F9;
     display: flex;
     align-items: center;
+
+    ul {
+      display: flex;
+      justify-content: space-between;
+
+      li {
+        display: flex;
+
+        img {
+          width: 40px;
+        }
+      }
+    }
   }
   
 </style>
